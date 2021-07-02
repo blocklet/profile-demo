2fc22ca5cb520dbe72a238ef8642a49b79614e22
wangshijun <wangshijun2010@gmail.com>
2021-07-03 06:35:07 +0800

    chore: use authenticator from blocklet sdk

--- api/libs/env.js
+++ api/libs/env.js
@@ -1,16 +1,5 @@
 /* eslint-disable operator-linebreak */
 module.exports = {
-  appId:
-    process.env.BLOCKLET_APP_ID ||
-    process.env.REACT_APP_APP_ID ||
-    process.env.GATSBY_APP_ID ||
-    process.env.APP_ID ||
-    process.env.appId ||
-    '',
-  appName:
-    process.env.REACT_APP_APP_NAME ||
-    process.env.GATSBY_APP_NAME ||
-    process.env.APP_NAME ||
-    process.env.appName ||
-    'Auth Demo',
+  appId: process.env.BLOCKLET_APP_ID || '',
+  appName: process.env.BLOCKLET_APP_NAME || 'Profile Demo',
 };
