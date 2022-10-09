9865b969e4c884ab91b96a7b597433e0d00ac8ed
wangqi <donotlb@gmail.com>
2022-10-10 00:51:27 +0800

    chore: update deps & remove styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
-import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+import { ThemeProvider } from '@arcblock/ux/lib/Theme';
 import { LocaleProvider } from '@arcblock/ux/lib/Locale/context';
-import { create } from '@arcblock/ux/lib/Theme';
-
 import { SessionProvider } from './libs/session';
 import { translations } from './locales';
 import Main from './page/main';
@@ -15,40 +11,14 @@ if (window.blocklet && window.blocklet.prefix) {
 }
 
 function App() {
-  const theme = create({
-    typography: {
-      fontSize: 14,
-      button: {
-        textTransform: 'none',
-      },
-    },
-    overrides: {
-      MuiTableRow: {
-        root: {
-          '&:nth-child(even)': {
-            backgroundColor: 'rgba(0, 0, 0, 0.02)',
-          },
-        },
-        head: {
-          backgroundColor: 'transparent',
-        },
-      },
-    },
-  });
-
   return (
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <StyledThemeProvider theme={theme}>
-          <LocaleProvider translations={translations}>
-            <SessionProvider serviceHost={prefix}>
-              <CssBaseline />
-              <Main />
-            </SessionProvider>
-          </LocaleProvider>
-        </StyledThemeProvider>
-      </ThemeProvider>
-    </StyledEngineProvider>
+    <ThemeProvider>
+      <LocaleProvider translations={translations}>
+        <SessionProvider serviceHost={prefix}>
+          <Main />
+        </SessionProvider>
+      </LocaleProvider>
+    </ThemeProvider>
   );
 }
 
