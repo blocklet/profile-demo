ef9586a0a7557cb5919c8230cd0a8a2eb6db301b
zhanghan <lancelot_lewis@163.com>
2024-07-10 13:41:49 +0800

    chore(deps): update deps

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { createBlockletPlugin } from 'vite-plugin-blocklet';
 
-console.log('------------------------------------');
-console.log(111);
-console.log('------------------------------------');
-
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
