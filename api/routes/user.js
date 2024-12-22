aa55b1c3db4680fff90cedfdf332b517fb7ab164
wangshijun <wangshijun2010@gmail.com>
2024-12-22 19:34:53 +0800

    chore: bump deps to latest

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,17 +1,23 @@
-const AuthService = require('@blocklet/sdk/service/auth');
-const middlewares = require('@blocklet/sdk/lib/middlewares');
+const AuthService = require("@blocklet/sdk/service/auth");
+const middlewares = require("@blocklet/sdk/lib/middlewares");
+const component = require("@blocklet/sdk/lib/component");
 
 const authClient = new AuthService();
 
 module.exports = {
   init(app) {
-    app.get('/api/did/user', middlewares.session(), async (req, res) => {
+    app.get("/api/did/user", middlewares.session(), async (req, res) => {
       res.json({
         user: req.user,
       });
     });
 
-    app.get('/api/user', middlewares.session(), async (req, res) => {
+    app.get("/api/owner", async (req, res) => {
+      const owner = await authClient.getOwner();
+      res.json({ owner });
+    });
+
+    app.get("/api/user", middlewares.session(), async (req, res) => {
       if (!req.user) {
         res.json({ user: null });
         return;
@@ -23,6 +29,37 @@ module.exports = {
       } catch (err) {
         console.error(err);
         res.json({ user: null });
+      }
+    });
+
+    app.get("/api/call1", async (req, res) => {
+      res.json({ source: "api", result: "call1" });
+    });
+
+    app.get("/api/call2", async (req, res) => {
+      const random = Math.random();
+      console.log("random", random);
+      if (random > 0.5) {
+        res.status(500).json({ error: "Random error" });
+      } else {
+        res.json({ source: "api", result: "call2" });
+      }
+    });
+
+    app.get("/api/component/:path", async (req, res) => {
+      try {
+        const result = await component.call(
+          {
+            name: process.env.BLOCKLET_COMPONENT_DID,
+            method: req.query.method || req.method,
+            path: `/api/${req.params.path}`,
+          },
+          { retries: 5 }
+        );
+        res.json({ source: "component", result: result.data });
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Component call failed" });
       }
     });
   },
